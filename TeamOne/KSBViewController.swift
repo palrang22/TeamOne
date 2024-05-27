9ace8a792914abcb2452096a90e081f5c7a56786
kseunghui@gmail.com <kseunghui@gmail.com>
2024-05-27 12:45:20 +0900

    Initial Commit

---
+++ TeamOne/KSBViewController.swift
@@ -0,0 +1,29 @@
+//
+//  KSBViewController.swift
+//  TeamOne
+//
+//  Created by 김승희 on 5/27/24.
+//
+
+import UIKit
+
+class KSBViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
